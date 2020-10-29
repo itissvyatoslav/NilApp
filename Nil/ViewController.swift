516427598db94e67053153dce54041d8120facfd
Shevchenko Svyatoslav <itissvyatoslav@mail.ru>
2020-10-29 14:58:39 +0700

    Initial Commit

---
+++ Nil/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nil
+//
+//  Created by Svyatoslav Vladimirovich on 29.10.2020.
+//  Copyright © 2020 Svyatoslav Vladimirovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
